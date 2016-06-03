6b44354ce1e51146a7acb1a3de7e2b3abfe0e0ff
JENNYPILAY <nashlyascepilay.com>
2016-06-03 17:00:56 -0500

    seagrego app

--- js/app1.js
+++ js/app1.js
@@ -1,8 +1,17 @@
+
+
+
+
+
+
 $(".botton").append("<button>buscar</button>");
 $(".botton").click(function()
 {
 	var opcion = $(".elemento").select();
 	if(opcion =="carga")
+
+
+
 	{
 	document.write("hoola");
 }
